f775d1571f891dd2da6d88927287d37cf5af5755
EC2 Default User <ec2-user@ip-172-31-47-213.ap-northeast-1.compute.internal>
2023-01-15 07:46:21 +0000

    [Add]addressデータベース作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,4 @@
 class Admin::CustomersController < ApplicationController
-  def index
-  end
 
   def show
   end
--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,31 @@
 class Public::AddressesController < ApplicationController
+
+  def create
+    @address = current_customer.addresses.new(addresses_params)
+    if @address.save
+      @address = current_customer.addresses.all
+    else
+      @address = current_customer.address.all
+      render :error
+    end
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def addresses_params
+    params.require(:address).permit(:name, :posttal_code, :address)
+  end
+
 end
