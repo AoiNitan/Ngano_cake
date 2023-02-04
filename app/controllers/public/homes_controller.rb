c96c92d56b9fce881985480a4a4c16c176130498
EC2 Default User <ec2-user@ip-172-31-47-213.ap-northeast-1.compute.internal>
2023-02-04 03:18:35 +0000

    [add]view

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,20 @@
 class Public::HomesController < ApplicationController
 
+  before_action :set_items, only: [:top]
+
   def top
+    @genres = Genre.all
+    @items = @new_items.first(4)
   end
 
   def about
   end
+
+  private
+
+  def set_items
+    @new_items = Item.all.order('created_at DESC')
+  end
+
 end
+
